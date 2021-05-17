7f834f3f56e1549540158a71dc0ad9ea937b23b9
milind31 <milind.rv@gmail.com>
2021-05-16 23:16:44 -0700

    hook up text fields in create post w firebase

--- roastbattles/src/components/create-post.js
+++ roastbattles/src/components/create-post.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import withStyles from '@material-ui/core/styles/withStyles';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { withStyles } from '@material-ui/styles';
 import PropTypes from 'prop-types';
+
 
 import firebase from 'firebase/app';
 import "firebase/auth";
@@ -10,7 +11,15 @@ import "firebase/firestore";
 import "firebase/auth";
 
 const styles = {
-    root: {
+    form: {
+        backgroundColor: '#242323',
+        alignItems: 'center'
+    },
+    button: {
+        backgroundColor: '#ed6c09'
+    },
+    fileUpload: {
+        alignItems: 'center'
     }
 }
 
@@ -19,66 +28,218 @@ class CreatePost extends Component {
     constructor(props) {
         super(props);
 
+        this.handleAuthChange = this.handleAuthChange.bind(this);
+        this.onChangeMusic = this.onChangeMusic.bind(this);
+        this.onChangeAge = this.onChangeAge.bind(this);
+        this.onChangeDayAsOtherPerson = this.onChangeDayAsOtherPerson.bind(this);
+        this.onChangeHobbies = this.onChangeHobbies.bind(this);
+        this.onChangePeeves = this.onChangePeeves.bind(this);
+        this.onChangeSelfRating = this.onChangeSelfRating.bind(this);
+        this.onChangeGuiltyPleasure = this.onChangeGuiltyPleasure.bind(this);
+        this.onChangeOtherInfo = this.onChangeOtherInfo.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            music: '',
+            age: '',
+            dayAsOtherPerson: '',
+            hobbies: '',
+            peeves: '',
+            selfRating: '',
+            guiltyPleasure: '',
+            otherInfo: '',
+        }
     }
 
     componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(this.handleAuthChange);
+    }
+
+    handleAuthChange(user) {
+        if (user) {
+            //user is logged in
+        } else {
+            //user is not logged in
+            window.location = '/signin';
+        }
+    }
+
+    onChangeMusic(e) {
+        this.setState({
+            music: e.target.value
+        });
+    }
+
+    onChangeAge(e) {
+        this.setState({
+            age: e.target.value
+        });
+    }
+
+    onChangeDayAsOtherPerson(e) {
+        this.setState({
+            dayAsOtherPerson: e.target.value
+        });
+    }
+
+    onChangeHobbies(e) {
+        this.setState({
+            hobbies: e.target.value
+        });
+    }
+
+    onChangePeeves(e) {
+        this.setState({
+            peeves: e.target.value
+        });
+    }
+
+    onChangeSelfRating(e) {
+        this.setState({
+            selfRating: e.target.value
+        });
+    }
+
+    onChangeGuiltyPleasure(e) {
+        this.setState({
+            selfRating: e.target.value
+        });
+    }
+
+    onChangeOtherInfo(e) {
+        this.setState({
+            selfRating: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        
+        const post = {
+            music: this.state.music,
+            age: this.state.age,
+            dayAsOtherPerson: this.state.dayAsOtherPerson,
+            hobbies: this.state.hobbies,
+            peeves: this.state.peeves,
+            selfRating: this.state.selfRating,
+            guiltyPleasure: this.state.guiltyPleasure,
+            otherInfo: this.state.otherInfo,
+        }
+        
+        var user = firebase.auth().currentUser;
+
+        firebase.firestore().collection("posts").doc(user.uid.toString()).set({post})
+        .then(() => {window.location = '/'})
+        .catch((err) => {console.log(err)})
     }
 
 
     render () {
+        //TODO: MAKE FILE UPLOAD REQUIRED!!
         const { classes } = this.props;
         return (
             <div>
-                <h2>Create post...</h2>
-                <div>
-                    <TextField
-                    id="filled-helperText"
-                    label="Helper text"
-                    defaultValue="Default Value"
-                    variant="filled"
-                    style={{backgroundColor: "#3b3b3b"}}
-                    InputProps={{style: {color: "white"}}}
-                    />
-                </div>
-                <div>
-                    <TextField
-                    id="filled-helperText"
-                    label="Helper text"
-                    defaultValue="Default Value"
-                    variant="filled"
-                    style={{backgroundColor: "#3b3b3b"}}
-                    InputProps={{style: {color: "white"}}}
-                    />
-                </div>
-                <div>
-                    <TextField
-                    id="filled-helperText"
-                    label="Helper text"
-                    defaultValue="Default Value"
-                    variant="filled"
-                    style={{backgroundColor: "#3b3b3b"}}
-                    InputProps={{style: {color: "white"}}}
-                    />
-                </div>
-                <div>
-                    <TextField
-                    id="filled-helperText"
-                    label="Helper text"
-                    defaultValue="Default Value"
-                    variant="filled"
-                    style={{backgroundColor: "#3b3b3b", marginBottom: '20px'}}
-                    InputProps={{style: {color: "white"}}}
-                    />
-                </div>
-                <Button color="primary" variant="outlined">Submit Form</Button>
+                <h1>Create post...</h1>
+                <Form className={classes.form}  onSubmit={this.onSubmit}>
 
+                { /*file upload*/ }
+                <Form.Group>
+                    <Form.Label>Upload images of yourself</Form.Label>
+                    <Form.File className={classes.fileUpload} id="exampleFormControlFile1" multiple/>
+                    <Form.Text className="text-muted">
+                            Required: please upload 1-5 images
+                    </Form.Text>
+                </Form.Group>
+
+                    { /*optional fields*/ }
+                    <Form.Label>
+                            The following are optional; feel free to answer as many or as few as you would like
+                    </Form.Label>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangeMusic} 
+                        value={this.state.music} 
+                        as="textarea" 
+                        rows={1} 
+                        placeholder="What music do you currently have on rotation?" />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangeAge} 
+                        value={this.state.age} 
+                        as="textarea" rows={1} 
+                        placeholder="How old are you?" />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangeDayAsOtherPerson} 
+                        value={this.state.dayAsOtherPerson} 
+                        as="textarea" rows={1} 
+                        placeholder="If you could spend a day as anyone, dead or alive, who would it be?" />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangeHobbies} 
+                        value={this.state.hobbies} 
+                        as="textarea" 
+                        rows={1} 
+                        placeholder="What do you enjoy doing in your spare time?" />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangePeeves}
+                        value={this.state.peeves} 
+                        as="textarea" 
+                        rows={1} 
+                        placeholder="What pisses you off the most?" />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangeSelfRating} 
+                        value={this.state.selfRating} 
+                        as="textarea" 
+                        rows={1} 
+                        placeholder="Rate your looks on a scale from 1-10" />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangeGuiltyPleasure} 
+                        value={this.state.guiltyPleasure} 
+                        as="textarea" 
+                        rows={1} 
+                        placeholder="What is you guilty pleasure?" />
+                    </Form.Group>
+
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control 
+                        onChange={this.onChangeOtherInfo}
+                        value={this.state.otherInfo}  
+                        as="textarea" 
+                        rows={3} 
+                        placeholder="Anything else you want to let people know..." />
+                    </Form.Group>
+                    
+                    { /*submit*/ }
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+
+                    </Form>
             </div>
         )
   }
 }
+
 CreatePost.propTypes = {
     classes: PropTypes.object.isRequired
 }
 
 
-export default (withStyles(styles)(CreatePost));
+export default withStyles(styles)(CreatePost);
